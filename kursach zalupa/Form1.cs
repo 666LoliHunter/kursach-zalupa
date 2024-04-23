9a3d7f9c5318b4b9d24b34bad57e117ef23af634
Жигмит Намжилов <2drots05@gmail.com>
2024-04-23 14:50:36 +0300

    upd code btn

--- kursach zalupa/Form1.cs
+++ kursach zalupa/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,41 @@ namespace kursach_zalupa
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // прочитать данные из БД
+            oleDbDataAdapter1.Fill(dataSet1.Tables[0]);
+
+            // можно и так:
+            // oleDbDataAdapter1.Fill(dataSet1.Tables["сontacts"]);
+        }
+
+        // завершение работы программы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            oleDbDataAdapter1.Update(dataSet1.Tables[0]);
+        }
+        // пользователь выделил строку и нажал <Delete>
+        private void dataGridView1_UserDeletingRow(object sender,
+
+        DataGridViewRowCancelEventArgs e)
+
+        {
+            DialogResult dr = MessageBox.Show(
+
+            "Запись будет удалена из БД.\nВыполнить?",
+            "Удаление записи",
+            MessageBoxButtons.OKCancel,
+            MessageBoxIcon.Warning,
+            MessageBoxDefaultButton.Button2);
+
+            if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+
         }
 
         private void oleDbConnection1_InfoMessage(object sender, System.Data.OleDb.OleDbInfoMessageEventArgs e)
